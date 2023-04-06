9d2dfbbc9b3aa81220cdb13c32505d35be0af963
Vasyl96 <93982881+vasyl-horaichuk@users.noreply.github.com>
2023-04-06 21:34:23 +0300

    update

--- contacts.js
+++ contacts.js
@@ -1,11 +1,11 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const FilePath = path.join(__dirname, './db');
+const FilePath = path.join(__dirname, 'db', 'contacts.json');
 
 const contactsPatch = fs
-  .readFile(FilePath)
-  .then(date => console.log(date.toString()))
-  .catch(err => console.error);
+  .readFile(FilePath, 'utf-8')
+  .then(date => console.log(date))
+  .catch(err => console.error(err));
 
 module.exports = contactsPatch;
