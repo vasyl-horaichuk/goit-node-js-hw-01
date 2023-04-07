8e380f23356f3153eebae733866a5aff46cdc9ea
Vasyl96 <93982881+vasyl-horaichuk@users.noreply.github.com>
2023-04-07 23:18:28 +0300

    update

--- contacts.js
+++ contacts.js
@@ -7,16 +7,21 @@ const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 function listContacts() {
   const data = fs
     .readFile(contactsPath, 'utf-8')
-    .then(date => consol.log(date))
+    .then(date => {
+      console.log(date.find(contact => contact.id === contactId));
+    })
     .catch(err => console.error(err));
-  return data;
+  // return data;
 }
 
 // Отримати конкретний контакт
 function getContactById(contactId) {
   const list = listContacts();
-  const result = list.find(contact => contact.id === contactId);
-  return result || null;
+  // const result = list.find(contact => contact.id === contactId);
+  // console.log(result);
+  // console.log(list);
+  // const result = list.find(item => item.id === contactId);
+  // return result || null;
 }
 
 function removeContact(contactId) {
