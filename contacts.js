d8a2b1e9c90ecee50432c3f8404d9fdb4d16cc5b
Vasyl96 <93982881+vasyl-horaichuk@users.noreply.github.com>
2023-04-07 23:57:43 +0300

    update

--- contacts.js
+++ contacts.js
@@ -4,25 +4,17 @@ const path = require('path');
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
 // Читаємо файл
-function listContacts() {
-  const data = fs
-    .readFile(contactsPath, 'utf-8')
-    .then(date => {
-      console.log(date.find(contact => contact.id === contactId));
-    })
-    .catch(err => console.error(err));
-  // return data;
-}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return consoleJSON.parse(data);
+};
 
 // Отримати конкретний контакт
-function getContactById(contactId) {
-  const list = listContacts();
-  // const result = list.find(contact => contact.id === contactId);
-  // console.log(result);
-  // console.log(list);
-  // const result = list.find(item => item.id === contactId);
-  // return result || null;
-}
+const getContactById = async contactId => {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+  return result || null;
+};
 
 function removeContact(contactId) {
   // ...твій код
