ba75afbec6c430f410630b72bcbb8ade183cc00c
Vasyl96 <93982881+vasyl-horaichuk@users.noreply.github.com>
2023-04-08 00:32:26 +0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const contacts = require('./contacts');
+// const argv = require('yargs').argv;
 
-contacts.listContacts();
-// contacts.getContactById('AeHIrLTr6JkxGE6SN-0Rw');
-// 'qdggE76Jtbfd9eWJHrssH';
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+      break;
+
+    case 'get':
+      const getContacts = await contacts.getContactById(contactId);
+      return console.log(getContacts);
+      break;
+
+    case 'add':
+      // ... name email phone
+      break;
+
+    case 'remove':
+      // ... id
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+};
+
+invokeAction({ action: 'read' });
+invokeAction({ action: 'get', id: 'qdggE76Jtbfd9eWJHrssH' });
+// invokeAction(argv);
